dbedb661a899279e5b91dd3721c245d9829732e1
Grace Chanice Iteka <85046852+Iteka006@users.noreply.github.com>
2023-11-24 01:32:13 +0200

    Add files via upload

---
+++ Iteka Grace Chanice 23656 CRUD/RegistrationsAuca/studentApp/src/main/java/com/example/studentapp/controller/CourseController.java
@@ -0,0 +1,101 @@
+package com.example.studentapp.controller;
+
+import com.example.studentapp.domain.Course;
+import com.example.studentapp.domain.Semester;
+import com.example.studentapp.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.rmi.RemoteException;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/courses/",consumes = {MediaType.APPLICATION_JSON_VALUE},
+        produces = {MediaType.APPLICATION_JSON_VALUE})
+public class CourseController {
+    @Autowired
+    private CourseService courseService;
+    @PostMapping(value = "/course")
+    public ResponseEntity<?> saveCourse(@RequestBody Course course) throws RemoteException {
+        if (course != null) {
+            Course savedCourse=courseService.insertCourse(course);
+            if (savedCourse != null) {
+                return new ResponseEntity<>("course Saved", HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>("failed", HttpStatus.BAD_REQUEST);
+            }
+        } else {
+            return new ResponseEntity<>("course is null", HttpStatus.BAD_REQUEST);
+
+        }
+    }
+    @GetMapping(value = "/course")
+    public ResponseEntity<?>allCourses() throws RemoteException {
+        List<Course> courses =courseService.selectAllCourses();
+        if(courses!=null){
+        return new ResponseEntity<>(courses, HttpStatus.OK);}
+        else{
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+    @GetMapping(value = "/course/{id}")
+    public ResponseEntity<?>selectCourse(@PathVariable ("id") Integer id) throws RemoteException{
+       Course course=courseService.selectCourse(id);
+        if(course!=null) {
+            return new ResponseEntity<>(course, HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>("course is null", HttpStatus.BAD_REQUEST);
+
+        }
+    }
+    @DeleteMapping("/course/{id}")
+    public ResponseEntity<?> deleteCourse(@PathVariable("id") Integer id) {
+        try {
+            courseService.deleteCourse(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @PutMapping("/course/{id}")
+    public ResponseEntity<?> updateCourse(@PathVariable("id") Integer id, @RequestBody Course updatedCourse) {
+        try {
+            Course existingCourse = courseService.selectCourse(id);
+
+            if (existingCourse != null) {
+                // Update the properties of the existing course with the new values
+                existingCourse.setCoursd(updatedCourse.getCoursd());
+                existingCourse.setTeacher(updatedCourse.getTeacher());
+                existingCourse.setSems(updatedCourse.getSems());
+                existingCourse.setAcademic(updatedCourse.getAcademic());
+                existingCourse.setStudents(updatedCourse.getStudents());
+                Course updatedCourseEntity = courseService.insertCourse(existingCourse);
+
+                if (updatedCourseEntity != null) {
+                    return new ResponseEntity<>("Course Updated", HttpStatus.OK);
+                } else {
+                    return new ResponseEntity<>("Update failed", HttpStatus.BAD_REQUEST);
+                }
+            } else {
+                return new ResponseEntity<>("Course not found", HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping(value = "/course/academic/{acaid}/{semid}")
+    public ResponseEntity<?>coursePerDepSem(@PathVariable("acaid") Integer acaid, @PathVariable("semid") Integer semid)throws RemoteException{
+        List<Course> courses=courseService.getCourseByDepAndSemester(acaid,semid);
+        return new ResponseEntity<>(courses,HttpStatus.OK);
+    }
+    @GetMapping(value = "/course/student/{stid}")
+    public ResponseEntity<?>CoursePerStud(@PathVariable("stid") Integer stid) throws RemoteException{
+        List<Course>courses=courseService.getCoursePerStudent(stid);
+        return new ResponseEntity<>(courses,HttpStatus.OK);
+    }
+}
---
+++ Iteka Grace Chanice 23656 CRUD/RegistrationsAuca/studentApp/src/main/java/com/example/studentapp/controller/SemesterController.java
@@ -0,0 +1,90 @@
+package com.example.studentapp.controller;
+
+import com.example.studentapp.domain.Semester;
+import com.example.studentapp.domain.Student;
+import com.example.studentapp.service.SemesterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.rmi.RemoteException;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/semesters/",consumes = {MediaType.APPLICATION_JSON_VALUE},
+        produces = {MediaType.APPLICATION_JSON_VALUE})
+public class SemesterController {
+    @Autowired
+    private SemesterService semesterService;
+    @PostMapping(value = "/semester")
+    public ResponseEntity<?> saveSemester(@RequestBody Semester semester) throws RemoteException {
+        if (semester != null) {
+           Semester savedSemester=semesterService.insertSemester(semester);
+            if (savedSemester != null) {
+                return new ResponseEntity<>("semester Saved", HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>("failed", HttpStatus.BAD_REQUEST);
+            }
+        } else {
+            return new ResponseEntity<>("semester is null", HttpStatus.BAD_REQUEST);
+
+        }
+    }
+    @GetMapping(value = "/semester")
+    public ResponseEntity<List<?>>allSemester() throws RemoteException {
+       List<Semester> semesters = semesterService.selectAllSemesters();
+            return new ResponseEntity<>(semesters, HttpStatus.OK);
+
+    }
+    @GetMapping(value = "/semester/{id}")
+    public ResponseEntity<?>selectSemester(@PathVariable ("id") Integer id) throws RemoteException{
+        Semester semester=semesterService.selectSemester(id);
+        if(semester!=null) {
+            return new ResponseEntity<>(semester, HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>("student is null", HttpStatus.BAD_REQUEST);
+
+        }
+    }
+
+    @PutMapping("/semester/{id}")
+    public ResponseEntity<?> updateSemester(@PathVariable("id") Integer id, @RequestBody Semester updatedSemester) {
+        try {
+            Semester existingSemester = semesterService.selectSemester(id);
+
+            if (existingSemester != null) {
+
+                existingSemester.setName(updatedSemester.getName());
+                existingSemester.setStartdate(updatedSemester.getStartdate());
+                existingSemester.setEnddate(updatedSemester.getEnddate());
+
+                Semester updatedSemesterEntity = semesterService.insertSemester(existingSemester);
+
+                if (updatedSemesterEntity != null) {
+                    return new ResponseEntity<>("Semester Updated", HttpStatus.OK);
+                } else {
+                    return new ResponseEntity<>("Update failed", HttpStatus.BAD_REQUEST);
+                }
+            } else {
+                return new ResponseEntity<>("Semester not found", HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+    @DeleteMapping("/semester/{id}")
+    public ResponseEntity<?> deleteSemester(@PathVariable("id") Integer id) {
+        try {
+            semesterService.deleteSemester(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+}
